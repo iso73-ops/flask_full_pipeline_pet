ee860ed8065901ea72f01651e95b962458cfc0fd
iso73-ops <hzubov2223@gmail.com>
2023-02-20 08:03:43 +0200

    First Commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    email = request.form['email']
+    save_data(name, email)
+    return render_template('submit.html', name=name, email=email)
+
+
+def save_data(name, email):
+    with open('files/data.txt', 'a') as f:
+        f.write(f'{name}, {email}\n')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
